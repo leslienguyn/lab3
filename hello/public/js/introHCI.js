3713a099d9cba1f6591e1ef21d6d52be3a84b91d
Leslie Nguyen <len007@ucsd.edu>
2021-01-21 11:50:23 -0800

    updating files

--- hello/public/js/introHCI.js
+++ hello/public/js/introHCI.js
@@ -2,12 +2,33 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+        initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// add any functionality and listeners you want here
+    $('#testjs').click(function(e) {
+		$('.jumbotron h1').text("i cant talk rn im doing hot girl stuff");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").text("Change this button text!");
+	});
+
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+  	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(description).hide();
+	}
 }
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,41 +2,33 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+        initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions. Too many questions is the Chan disease. The best way is just to observe the noise of the world. The answer to your questions? Ask your own heart.");
+    $('#testjs').click(function(e) {
+		$('.jumbotron h1').text("i cant talk rn im doing hot girl stuff");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").text("Change this button text!");
 	});
 
-	$("#testjs").text("Please wait...");
-	$(".jumbotron p").addClass("active");
-	$(".jumbotron p").toggleClass("active");
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
-	$("a.thumbnail").click(projectClick);
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+  	$("a.thumbnail").click(projectClick);
 }
 
 function projectClick(e) {
-    e.preventDefault();
-	
-	var containingProject = $(this).closest(".project");
-	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-	
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
 	var containingProject = $(this).closest(".project");
 	var description = $(containingProject).find(".project-description");
-	
-
-    if (description.length == 1) {
-	   $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
-    } else {
-	   //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-	   $(description).hide();
+	if (description.length == 0) {
+	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(".project-description").toggle();
 	}
-	
-	
 }
